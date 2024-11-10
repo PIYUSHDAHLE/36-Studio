37b20b44bf5f0b0725c4548333968908a5b6126a
PIYUSHDAHLE <piyushdahle@gmail.com>
2024-11-10 21:13:19 +0530

    All Basic Setup Of React Vite and Tailwind CSS

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div className='flex justify-center items-center w-full h-[10vh] bg-black text-white '>
+      <h1 className='text-4xl font-thin'>Hello, world!</h1>
+    </div>
+  )
+}
+
+export default App
